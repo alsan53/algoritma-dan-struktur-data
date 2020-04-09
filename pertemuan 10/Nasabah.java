08f528b2fa243908f7ba0bc3b9e728c5cca74818
alsan53 <58768949+alsan53@users.noreply.github.com>
2020-04-09 12:03:53 +0700

    Add files via upload

---
+++ pertemuan 10/Nasabah.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktikum10Queue;
+public class Nasabah {
+    String noRekening, nama;
+    Nasabah(String noRek, String nm){
+        noRekening = noRek;
+        nama = nm;
+    }
+    Nasabah(){
+        
+    }
+    void print(){
+        System.out.println("Nama Nasabah\t: " + nama);
+        System.out.println("Rekening\t: " + noRekening);
+        System.out.println("------------------------");
+    }
+}
